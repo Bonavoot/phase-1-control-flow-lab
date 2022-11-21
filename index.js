eb6bf5b3eb7445f1c41baef8d6fe5ee4dc31af33
Joseph <Jbonventre95@gmail.com>
2022-11-20 19:11:23 -0500

    commit tree 2f0141e92401f7ff4c817785d8c397a2f65a019d

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if(someNumber <= 400){
+    return "This one is on me!"
+  } else if(someNumber >= 2000 && someNumber <= 2500){
+    return "I will gladly take your thirty bucks."
+
+  } else if(someNumber >= 2500){
+    return "No can do."
+
+  } else {
+    return "That will be twenty bucks."
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good." : "No go."
   // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch(tip){
+    case "generous":
+      tip = "Thank you so much."
+      break;
+      case "not as generous": 
+      tip = "Thank you."
+      break;
+      default: tip = "Bye."
+      
+  }
+
+  return tip;
+
 }
